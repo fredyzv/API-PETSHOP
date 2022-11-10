fd07a506a65b858d4d80b97755fbedd8f192029d
Fredy <fzuniga2019@hotmail.com>
2022-11-10 16:32:47 -0500

    prueba

--- src/main/java/com/petshop/model/Producto.java
+++ src/main/java/com/petshop/model/Producto.java
@@ -1,4 +1,13 @@
 package com.petshop.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name="producto")
 public class Producto {
+
 }
